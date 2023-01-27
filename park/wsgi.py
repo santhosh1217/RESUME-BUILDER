8955479b00067dbcb8e2028f924121171bb51de0
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2023-01-27 18:19:52 +0530

    Update wsgi.py

--- park/wsgi.py
+++ park/wsgi.py
@@ -2,11 +2,11 @@ import os
 import sys
 
 # assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/myusername/mysite'
+path = '/home/santhosh1217/park'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'park.settings'
 
 ## Uncomment the lines below depending on your Django version
 ###### then, for Django >=1.5:
