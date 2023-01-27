a7a18b2ffb6c913aeef0742a378b0631f67fc7f5
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2023-01-27 17:58:16 +0530

    Update settings.py

--- park/settings.py
+++ park/settings.py
@@ -80,12 +80,8 @@ WSGI_APPLICATION = 'park.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'JRKVpiAJ5j5XaBEMtJc2',
-        'HOST': 'containers-us-west-165.railway.app',
-        'PORT': '8067',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
