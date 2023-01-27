1bb38deaea9701096e7e794ded2d5affddf1b05b
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2023-01-27 18:12:32 +0530

    Create wsgi.py

--- park/wsgi.py
+++ park/wsgi.py
@@ -1,18 +1,14 @@
-"""
-WSGI config for park project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
+# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
+path = '/home/myusername/mysite'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+
+## Uncomment the lines below depending on your Django version
+###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'park.settings')
-
 application = get_wsgi_application()
-app = application
-
