44d2cc40679b7057b135a6e2f7093cbf94d44beb
santhosh1217 <79699124+santhosh1217@users.noreply.github.com>
2023-04-05 19:07:25 +0530

    Update wsgi.py

--- park/wsgi.py
+++ park/wsgi.py
@@ -1,14 +1,9 @@
+
 import os
-import sys
 
-# assuming your Django settings file is at '/home/myusername/mysite/mysite/settings.py'
-path = '/home/santhosh1217/park'
-if path not in sys.path:
-    sys.path.insert(0, path)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'park.settings'
-
-## Uncomment the lines below depending on your Django version
-###### then, for Django >=1.5:
 from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'park.settings')
+
 application = get_wsgi_application()
+
